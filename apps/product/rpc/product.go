601760a141afedd25a8976585a29267c04474f52
yflian <yunfeng.lian@miotech.com>
2023-08-23 11:58:38 +0800

    fix: cache\localCache

--- apps/product/rpc/product.go
+++ apps/product/rpc/product.go
@@ -23,7 +23,7 @@ func main() {
 
 	var c config.Config
 	conf.MustLoad(*configFile, &c)
-	ctx := svc.NewServiceContext(c)
+	ctx := svc.NewServiceContext(&c)
 
 	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
 		product.RegisterProductServer(grpcServer, server.NewProductServer(ctx))
